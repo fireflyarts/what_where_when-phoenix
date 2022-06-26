fb12a533a0616cc0d7ed237a202805f501c8eea7
Donald Guy <donaldguy@hey.com>
2022-06-26 15:43:55 -0400

    Add minified css build to the deploy script

--- mix.exs
+++ mix.exs
@@ -57,7 +57,7 @@ defmodule WhatWhereWhen.MixProject do
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
-      "assets.deploy": ["esbuild default --minify", "phx.digest"]
+      "assets.deploy": ["esbuild default --minify","tailwind default --minify", "phx.digest"]
     ]
   end
 
